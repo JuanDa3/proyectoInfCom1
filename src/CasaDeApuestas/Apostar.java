7de7a12d7701f85d3d537ae901f59b1df7ce7f4f
SyNhaA <91633578+SyNhaA@users.noreply.github.com>
2021-10-11 19:17:06 -0500

    Atributo cuenta en la clase Apostar

--- src/CasaDeApuestas/Apostar.java
+++ src/CasaDeApuestas/Apostar.java
@@ -1,43 +1,45 @@
 package CasaDeApuestas;
 
 public class Apostar {
-	
+
 	int numeroCuenta;
 	char tipoApuesta;
 	int numeroCualApostar;
+	Cuenta cuenta;
 
-	public Apostar(int numeroCuenta, char tipoApuesta, int numeroCualApostar) {
+	public Apostar(int numeroCuenta, char tipoApuesta, int numeroCualApostar, Cuenta cuenta) {
 		super();
 		this.numeroCuenta = numeroCuenta;
 		this.tipoApuesta = tipoApuesta;
 		this.numeroCualApostar = numeroCualApostar;
+		this.cuenta = cuenta;
 	}
 
 	public int getNumeroCuenta() {
 		return numeroCuenta;
 	}
-	
+
 	public void setNumeroCuenta(int numeroCuenta) {
 		this.numeroCuenta = numeroCuenta;
 	}
-	
+
 	public char getTipoApuesta() {
 		return tipoApuesta;
 	}
-	
+
 	public void setTipoApuesta(char tipoApuesta) {
 		this.tipoApuesta = tipoApuesta;
 	}
-	
+
 	public int getNumeroCualApostar() {
 		return numeroCualApostar;
 	}
-	
+
 	public void setNumeroCualApostar(int numeroCualApostar) {
 		this.numeroCualApostar = numeroCualApostar;
 	}
 
-	
+
 	@Override
 	public String toString() {
 		return "Apostar [numeroCuenta=" + numeroCuenta + ", tipoApuesta=" + tipoApuesta + ", numeroCualApostar="
@@ -45,5 +47,5 @@ public class Apostar {
 	}
 
 
-	
+
 }
